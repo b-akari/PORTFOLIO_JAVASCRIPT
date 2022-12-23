4cb8e58255809c3c4ab420906f66d2a6875ea749
b-akari <108397871+b-akari@users.noreply.github.com>
2022-12-23 16:56:13 +0800

    .

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-hamburger = document.querySelector(".hamburger");
-hamburger.onclick = function(){
-    navBar = document.querySelector(".nav-bar");
-    navBar.classList.toggle("active");
-}
+// hamburger = document.querySelector(".hamburger");
+// hamburger.onclick = function(){
+//     navBar = document.querySelector(".nav-bar");
+//     navBar.classList.toggle("active");
+// }
+
+const hamburger = document.querySelector('.hamburger');
+const navBar = document.querySelector('.nav-bar');
+const content = document.querySelector(".nav-bar ul");
+
+hamburger.addEventListener('click', () => {
+  navBar.classList.toggle('active');
+});
+
+// content.addEventListener("click", () => {
+//     navBar.classList.toggle("hide");
+// })
